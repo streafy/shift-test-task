1e70b1293eb633ef945b23597b40c013ab72b7a0
streafy <streafyhs@gmail.com>
2024-01-28 08:21:19 +0700

    fix: replace flow with stateflow in viewmodel

--- app/src/main/java/com/streafy/shifttesttask/presentation/userlist/UserListViewModel.kt
+++ app/src/main/java/com/streafy/shifttesttask/presentation/userlist/UserListViewModel.kt
@@ -2,9 +2,11 @@ package com.streafy.shifttesttask.presentation.userlist
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.cachedIn
+import androidx.paging.PagingData
 import com.streafy.shifttesttask.domain.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,5 +14,9 @@ class UserListViewModel @Inject constructor(
     repository: UserRepository
 ) : ViewModel() {
 
-    val state = repository.getUsers().cachedIn(viewModelScope)
+    val state = repository.getUsers().stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.Lazily,
+        initialValue = PagingData.empty()
+    )
 }
