647c856e2f26ab931bcbb77e47ff7aa1552e9f18
streafy <streafyhs@gmail.com>
2024-01-28 11:26:45 +0700

    feat: add swipe to refresh

--- app/src/main/java/com/streafy/shifttesttask/presentation/userlist/UserListScreen.kt
+++ app/src/main/java/com/streafy/shifttesttask/presentation/userlist/UserListScreen.kt
@@ -9,10 +9,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,30 +28,45 @@ import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.streafy.shifttesttask.domain.entity.User
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UserListScreen(
     viewModel: UserListViewModel = viewModel()
 ) {
     val users = viewModel.state.collectAsLazyPagingItems()
 
+    val state = rememberPullToRefreshState()
+    if (state.isRefreshing) {
+        users.refresh()
+        state.endRefresh()
+    }
+
     val context = LocalContext.current
     LaunchedEffect(key1 = users.loadState) {
         if (users.loadState.refresh is LoadState.Error) {
             Toast.makeText(
                 context,
-                "Error: ${(users.loadState.refresh as LoadState.Error).error.message}",
+                "Error occurred, swipe down to retry",
                 Toast.LENGTH_LONG
             ).show()
         }
     }
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        if (users.loadState.refresh is LoadState.Loading) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(state.nestedScrollConnection)
+    ) {
+        if (users.loadState.refresh is LoadState.Loading || state.isRefreshing) {
             CircularProgressIndicator(
                 modifier = Modifier.align(Alignment.Center)
             )
         } else {
             UserList(users)
+            PullToRefreshContainer(
+                modifier = Modifier.align(Alignment.TopCenter),
+                state = state,
+            )
         }
     }
 }
@@ -57,6 +78,13 @@ private fun UserList(users: LazyPagingItems<User>) {
         verticalArrangement = Arrangement.spacedBy(12.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        item {
+            Text(
+                text = "Users",
+                modifier = Modifier.padding(top = 12.dp),
+                style = MaterialTheme.typography.headlineLarge
+            )
+        }
         items(users.itemCount, key = { index -> users[index]?.id ?: -1 }) { index ->
             val user = users[index]
             if (user != null) {
