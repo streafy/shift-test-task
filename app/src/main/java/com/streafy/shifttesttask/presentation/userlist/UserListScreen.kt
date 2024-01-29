541dc3446ac5f5d69b49fbbe837d17d318a2ce87
streafy <streafyhs@gmail.com>
2024-01-30 00:29:16 +0700

    fix: center user card and make it 2 times smaller in landscape orientation

--- app/src/main/java/com/streafy/shifttesttask/presentation/userlist/UserListScreen.kt
+++ app/src/main/java/com/streafy/shifttesttask/presentation/userlist/UserListScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -81,7 +82,9 @@ private fun UserList(
     onUserClick: (user: User) -> Unit
 ) {
     LazyColumn(
-        Modifier.padding(horizontal = 16.dp),
+        Modifier
+            .padding(horizontal = 16.dp)
+            .fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(12.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -94,10 +97,15 @@ private fun UserList(
         }
         items(users.itemCount) { index ->
             val user = users[index]
+
+            val orientation = LocalConfiguration.current.orientation
+            val cardWidthFraction =
+                if (orientation == Configuration.ORIENTATION_LANDSCAPE) 0.5f else 1f
+
             if (user != null) {
                 UserCard(
                     user = user,
-                    modifier = Modifier.fillMaxWidth(),
+                    cardWidthFraction = cardWidthFraction,
                     onClick = { onUserClick(user) }
                 )
             } else {
