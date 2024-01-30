73a7e1fe94c828445b79779f553edde771ec05ac
streafy <streafyhs@gmail.com>
2024-01-30 20:14:55 +0700

    fix: convert date string from api to user readable format

--- app/src/main/java/com/streafy/shifttesttask/data/mappers/UserMappers.kt
+++ app/src/main/java/com/streafy/shifttesttask/data/mappers/UserMappers.kt
@@ -6,6 +6,8 @@ import com.streafy.shifttesttask.data.remote.model.RemoteResponse
 import com.streafy.shifttesttask.data.utils.Config
 import com.streafy.shifttesttask.domain.entity.User
 import com.streafy.shifttesttask.domain.entity.UserWithDetails
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 fun RemoteResponse.toUserEntityList() =
     results.mapIndexed { index, userDto ->
@@ -44,11 +46,16 @@ fun UserEntity.toUserWithDetails() =
         photoUri = pictureUri,
         address = location,
         phoneNumber = phone,
-        dateOfBirth = dateOfBirth,
-        registered = registered,
+        dateOfBirth = LocalDateTime.parse(dateOfBirth, formatter).toDateString(),
+        registered = LocalDateTime.parse(registered, formatter).toDateString(),
         gender = gender,
         email = email
     )
 
 private fun LocationDto.toAddressString() =
     "$city, $country"
+
+private fun LocalDateTime.toDateString() =
+    "$dayOfMonth.$monthValue.$year"
+
+private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
